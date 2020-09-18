cfe9453b5bd0ade1b97198e63bca34340c7ce675
shukriev <dark00v3@gmail.com>
2020-09-18 14:08:34 +0300

    Clear project

--- task2/exception/ConfigException.py
+++
@@ -1,8 +0,0 @@
-class ConfigException(Exception):
-    """Configuration Exception"""
-    pass
-
-
-class ConfigNotExisting(ConfigException):
-    """Configuration Not Existing exception"""
-    pass
